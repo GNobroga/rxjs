10356f8647deed93a74f47cfb205ad582a0ad483
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-16 01:44:31 -0300

    [ADD] Functors

---
+++ functional/functors.ts
@@ -0,0 +1,8 @@
+/*
+    # Functors 
+
+        São objetos que implementam a função (map), ou seja, que podem ser
+        mapeados (transformados) para um valor diferente, sem alterar o tamanho
+        original.
+
+*/
--- functional/lazy-evaluation.ts
+++ functional/lazy-evaluation.ts
@@ -1,4 +1,35 @@
 /*
+    # lazy Evaluation
     
+        É uma estratégia de avaliação em programação em que a 
+        computação de uma expressão é adiada até o momento em que o
+        resultado é realmente necessário.
 
 */
+
+
+// Nesse exemplo sempre que eu precisar do valor, vou ter que aguardar 5 segundos.
+function powEager(a: number, b: number) {
+    const end = Date.now() + 5000;
+    while (Date.now() < end) {};
+    const pow = Math.pow(a, b);
+    return pow;
+}
+
+// Nesse exemplo sempre que eu precisar do valor,
+// eu vou apenar chamar a função carrying sem precisar processar o calculo novamente.
+function powLazy(a: number, b: number) {
+    const end = Date.now() + 5000;
+    while (Date.now() < end) {};
+    const pow = Math.pow(a, b);
+    return function() {
+        return pow;
+    }
+}
+
+console.log('Pow eager: ' + powEager(10, 2));
+
+const calcLazy = powLazy(10, 2);
+
+console.log('Pow lazy 1: ' + calcLazy());
+console.log('Pow lazy 2: ' + calcLazy());
