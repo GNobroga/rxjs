9adec2ca03b1f213d936e37eaf33e03536206e10
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-16 01:27:02 -0300

    [UPDATE] Modificando README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# RxJs
+# Tip
 
-Esse repositório conterá dicas de programação funcional e reatividade 
+Repositório que contém algumas dicas relacionadas a programação funcional e reatividade utilizando RxJs. Estou me baseando na documentação e livros pra construir a ideia de cada conceito.
--- functional/currying.ts
+++ functional/currying.ts
@@ -1,4 +1,22 @@
 /*
     # Currying
 
+        A ideia da currying é passar parâmetros parcialmente para uma função através de chaamdas 
+        para outras funções;
 */
+
+function startCalc(a: number) {
+    return function(add: number) {
+        return function (mul: number) {
+            return (a + add) * mul;
+        }
+    }
+}
+
+console.log('Not arrow function: ' + startCalc(5)(5)(5)); // Output 50
+
+// Reduzindo a função acima
+
+const startCalcWithArrowFunction = (a: number) => (add: number) => (mul: number) => (a + add) * mul;
+
+console.log('With arrow function: ' + startCalcWithArrowFunction(5)(5)(5)) // Output 50
